7794401f5bae50b9b7d5abb7954cfd25229d039d
23127jk <23127jk@hvhs.school.nz>
2026-05-19 15:15:56 +1200

    sort of set up

--- fb_io.js
+++ fb_io.js
@@ -6,10 +6,45 @@
  **                                                          **
  **************************************************************
  **************************************************************/
-function fb_authenticate(){
-    // authenticate with Google
+ var GLOBAL_user;
+function fb_login() {
+    console.log("logging in")
+  authenticationListener = firebase.auth().onAuthStateChanged(fb_handleLogin);
+  firebase.auth().onAuthStateChanged((user) => { 
+     if (user) {
+    console.log("logged in")
+    console.log(user)
+    var uid = user.uid;
+  } else {
+    console.log("not logged in")
+    // user is signed out
+    }
+  });
 }
 
+function fb_handleLogin(_user) {
+  if (_user) {
+    console.log("user is logged in")
+    GLOBAL_user = _user; // save the user object to a global varible
+  } else {
+    console.log("user is NOT logged in - starting the popup process")
+    fb_popupLogin();
+  }
+}
+function fb_popupLogin() {
+  var provider = new firebase.auth.GoogleAuthProvider();
+
+  firebase.auth().signInWithPopup(provider).then((result) => {
+    GLOBAL_user = result.user; //save the user details object to a globel variable
+    console.log("User has logged in")
+  });
+}        
+var authenticationListener
+function fb_logout() {
+  authenticationListener();
+  firebase.auth().signOut();
+  console.log("logged out (hopefully)")
+}
 function fb_error(error){
   console.log("there was an error reading the message");
   console.error(error);
--- script.js
+++ script.js
@@ -1,16 +1,36 @@
+const HTML_OUTPUT = document.getElementById("statusMessage");
 
 console.log("Running Sal's Strawberries")
 
-function writeForm(){
-    // Get the form data
-    const favoriteFruit = document.getElementById("favoriteFruit").value;
+function fb_error(error){
+  console.log("there was an error reading the message");
+  console.error(error);
 }
 
-function helloWorld() {
-  console.log("Running helloWorld()")
+function writeForm(){
+    // Get the form data
+    console.log("reading info")
+    firebase.database().ref('/').child('message').once('value')
+    var favFruit = document.getElementById("favoriteFruit").value;
+}
+
+
   firebase.database().ref('/').set(
     {
       message: 'hello world'
     }
   )
+foodTable ={
+  users:{
+    nick: "noodles",
+    tom: "bread"
+  }
 }
+
+
+firebase.database().ref('/').set(foodTable)
+//firebase.database().ref('/foodTable/users/alex').set("berrys")
+//firebase.database().ref('/foodTable/users/tom').set("cheese")
+
+//console.log(users+ "likes"+ favFruit)
+
